3468ef0cadb2f35a8ff616f5339a2d4809881180
Dimitri Gilbert <dimitri.gilbert@gmail.com>
2022-09-03 13:55:39 +0200

    execution group

--- src/start_command.sh
+++ src/start_command.sh
@@ -1,13 +1,21 @@
-inspect_args
+# inspect_args
+# shellcheck source=../.gtfw
 
 source "${args[--directory]}/${args[--gtfw-file]}"
 
-if [ "${args[--execute]}" != "" ]; then
-  eval "gtfw_execute+=(${args[--execute]})";
+exgp="gtfw_execute";
+if [ "${args[--group]}" != "" ]; then
+  exgp+="_${args[--group]}";
 fi
 
-for gtfw_ex in "${gtfw_execute[@]}"; do
+if [ "${args[--execute]}" != "" ]; then
+  eval "${exgp}+=(${args[--execute]})";
+fi
+
+toexarr=()
+eval "toexarr=(\"\${${exgp}[@]}\")";
+
+for gtfw_ex in "${toexarr[@]}"; do
   # echo "executing : $gtfw_ex";
   eval "$gtfw_ex";
 done;
-
